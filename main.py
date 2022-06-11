aba0db5e0b1b04b4229f7eb9a5f0c5160166dc5f
Mengistu <62506916+sgetme@users.noreply.github.com>
2022-06-11 13:35:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 
 import math
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 
 
+
+Builder.laod_string(""" 
+<DataTables>:
+  Widget:
+    canvas.before:
+      Color: 
+        rgba: (1, 1, 1, 1)
+       Rectangle:
+        pos: self.pos
+        size: self.size
+        
+ 
+
+
+""")
